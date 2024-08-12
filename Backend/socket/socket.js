4131640ea55812cf37ce6b79eb4c37ad35088eda
Divyaraj <divyaraj.com>
2024-08-12 16:17:45 +0530

    update

--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: ['https://Divyaraj-Gohil.github.io/Chat-App', 'https://divyaraj-gohil-chat.netlify.app', 'http://localhost:5173'],
+        origin: ['https://divyaraj-gohil.github.io', 'https://divyaraj-gohil-chat.netlify.app', 'http://localhost:5173'],
         methods: ['GET', 'POST'],
         credentials: true
     }
